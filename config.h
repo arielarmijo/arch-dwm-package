b01a86b2bf60ea94a9b4d7e44a388876d3ff2232
cochayuyo <tumorlike@gmail.com>
2019-06-17 22:37:34 -0400

    Systray and statuscolors patches merged

--- config.h
+++ config.h
@@ -16,10 +16,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]         = "#ff0000";
+static const char col_yellow[]      = "#ffff00";
+static const char col_white[]       = "#ffffff";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan, col_cyan  },
+	/*				   fg         bg         border   */
+	[SchemeNorm]   = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]    = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeUrg]    = { col_red  , col_gray1, col_gray1 },
 };
 
 /* tagging */
